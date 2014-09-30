6ac98bab2649cc39a903e13f6478198c8a394623
Hiromu Yakura <hiromu1996@gmail.com>
2014-09-30 16:18:33 -0700

    initial commit with stf reader/writer and mfcc

---
+++ mfcc.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import numpy
+import scipy.fftpack
+import scipy.interpolate
+import scipy.linalg
+import sys
+
+from stf import STF
+
+class MFCC:
+    def __init__(self, nfft, frequency, dimension = 32):
+        self.nfft = nfft
+        self.frequency = frequency
+        self.dimension = dimension
+
+        self.fscale = numpy.fft.fftfreq(self.nfft, d = 1.0 / self.frequency)[: self.nfft / 2]
+        self.filterbank, self.fcenters = self.melFilterBank()
+
+    def hz2mel(self, f):
+        return 1127.01048 * numpy.log(f / 700.0 + 1.0)
+
+    def mel2hz(self, m):
+        return 700.0 * (numpy.exp(m / 1127.01048) - 1.0)
+
+    def melFilterBank(self):
+        fmax = self.frequency / 2
+        melmax = self.hz2mel(fmax)
+
+        nmax = self.nfft / 2
+        df = self.frequency / self.nfft
+
+        dmel = melmax / (self.dimension + 1)
+        melcenters = numpy.arange(1, self.dimension + 1) * dmel
+        fcenters = self.mel2hz(melcenters)
+
+        indexcenter = numpy.round(fcenters / df)
+        indexstart = numpy.hstack(([0], indexcenter[0: self.dimension - 1]))
+        indexstop = numpy.hstack((indexcenter[1: self.dimension], [nmax]))
+
+        filterbank = numpy.zeros((self.dimension, nmax))
+        for c in numpy.arange(0, self.dimension):
+            increment = 1.0 / (indexcenter[c] - indexstart[c])
+            for i in numpy.arange(indexstart[c], indexcenter[c]):
+                filterbank[c, i] = (i - indexstart[c]) * increment
+            decrement = 1.0 / (indexstop[c] - indexcenter[c])
+            for i in numpy.arange(indexcenter[c], indexstop[c]):
+                filterbank[c, i] = 1.0 - ((i - indexcenter[c]) * decrement)
+            filterbank[c] /= (indexstop[c] - indexstart[c]) / 2
+
+        return filterbank, fcenters
+
+    def mfcc(self, spectrum):
+        mspectrum = numpy.log10(numpy.dot(spectrum, self.filterbank.transpose()))
+        return scipy.fftpack.dct(mspectrum, norm = 'ortho')
+
+    def imfcc(self, mfcc):
+        mspectrum = scipy.fftpack.idct(mfcc, norm = 'ortho')
+        tck = scipy.interpolate.splrep(self.fcenters, numpy.power(10, mspectrum))
+        return scipy.interpolate.splev(self.fscale, tck)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage: %s <stf_file>' % sys.argv[0]
+        sys.exit()
+
+    stf = STF()
+    stf.loadfile(sys.argv[1])
+
+    mfcc = MFCC(stf.SPEC.shape[1] * 2, stf.frequency)
+    res = mfcc.mfcc(stf.SPEC[stf.SPEC.shape[0] / 5])
+    spec = mfcc.imfcc(res)
+
+    print res
+
+    import pylab
+
+    pylab.subplot(211)
+    pylab.plot(stf.SPEC[stf.SPEC.shape[0] / 5])
+    pylab.ylim(0, 1.2)
+    pylab.subplot(212)
+    pylab.plot(spec)
+    pylab.ylim(0, 1.2)
+    pylab.show()
---
+++ stf.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import numpy
+import os
+import struct
+import sys
+
+class STF:
+    def __init__(self):
+        self.endian = '>'
+        self.chunks = ['APSG']
+
+    def loadfile(self, filename):
+        with open(filename, 'rb') as stf_file:
+            self.load(stf_file)
+
+    def load(self, stf_file):
+        filesize = os.fstat(stf_file.fileno()).st_size
+
+        while stf_file.tell() < filesize:
+            chunk = stf_file.read(4)
+
+            if chunk == 'STRT':
+                if stf_file.read(2) == '\xff\xfe':
+                    self.endian = '<'
+                chunk_size, self.version, self.channel, self.frequency = struct.unpack(self.endian + 'IHHI', stf_file.read(12))
+            else:
+                chunk_size, = struct.unpack(self.endian + 'I', stf_file.read(4))
+
+                if chunk == 'CHKL' or chunk == 'NXFL':
+                    data = stf_file.read(chunk_size)
+                    if chunk == 'CHKL':
+                        self.chunks += [data[i: i + 4] for i in range(0, chunk_size, 4) if data[i: i + 4] not in self.chunks]
+                else:
+                    self.shift_length, frame_count, argument, self.bit_size, self.weight, data_size = struct.unpack(self.endian + 'dIIHdI', stf_file.read(30))
+                    data = stf_file.read(data_size)
+
+                    element = data_size / (self.bit_size / 8)
+                    matrix = numpy.fromstring(data, count = element)
+
+                    for c in self.chunks:
+                        if chunk == c:
+                            if element / frame_count == 1:
+                                self.__dict__[c.strip()] = matrix
+                            else:
+                                self.__dict__[c.strip()] = matrix.reshape((frame_count, element / frame_count))
+                            break
+
+        for c in self.chunks:
+            if c.strip() not in self.__dict__:
+                self.__dict__[c.strip()] = None
+
+    def savefile(self, filename):
+        with open(filename, 'wb') as stf_file:
+            self.save(stf_file)
+
+    def save(self, stf_file):
+        stf_file.write('STRT')
+        if self.endian == '>':
+            stf_file.write('\xfe\xff')
+        elif self.endian == '<':
+            stf_file.write('\xff\xfe')
+        stf_file.write(struct.pack(self.endian + 'IHHI', 8, self.version, self.channel, self.frequency))
+
+        stf_file.write('CHKL')
+        stf_file.write(struct.pack(self.endian + 'I', len(''.join(self.chunks))) + ''.join(self.chunks))
+
+        for c in self.chunks:
+            if self.__dict__[c.strip()] == None:
+                continue
+
+            matrix = self.__dict__[c.strip()]
+            if len(matrix.shape) == 1:
+                argument = 1
+            else:
+                argument = matrix.shape[1]
+            data_size = matrix.shape[0] * argument * 8
+
+            header = struct.pack(self.endian + 'dIIHdI', self.shift_length, matrix.shape[0], argument, self.bit_size, self.weight, data_size)
+            stf_file.write(c + struct.pack(self.endian + 'I', len(header) + data_size) + header)
+
+            for i in xrange(matrix.shape[0]):
+                if argument == 1:
+                    stf_file.write(struct.pack(self.endian + 'd', matrix[i]))
+                else:
+                    for j in xrange(matrix.shape[1]):
+                        stf_file.write(struct.pack(self.endian + 'd', matrix[i, j]))
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage: %s <stf_file>' % sys.argv[0]
+        sys.exit()
+
+    stf = STF(sys.argv[1])
+    print stf.F0
