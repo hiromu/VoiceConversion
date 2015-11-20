13495fd04710c487109851fa1531a7b2a130ec4b
Hiromu Yakura <hiromu1996@gmail.com>
2015-11-20 12:48:23 +0900

    add dtw models of jnas pb sentences

--- convert_evgmm.py
+++ convert_evgmm.py
@@ -40,9 +40,6 @@ if __name__ == '__main__':
     output_mfcc = numpy.array([evgmm.convert(source_data[frame])[0] for frame in xrange(source_data.shape[0])])
     output_spec = numpy.array([mfcc.imfcc(output_mfcc[frame]) for frame in xrange(output_mfcc.shape[0])])
 
-    source.SPEC = output_spec
-    source.savefile(sys.argv[5])
-
     f0_data = []
     for i in source.F0:
         if i == 0:
@@ -51,3 +48,5 @@ if __name__ == '__main__':
             f0_data.append(math.e ** ((math.log(i) - math.log(f0[0][0])) * math.log(f0[1][1]) / math.log(f0[1][0]) + math.log(f0[0][1])))
 
     source.F0 = numpy.array(f0_data)
+    source.SPEC = output_spec
+    source.savefile(sys.argv[5])
